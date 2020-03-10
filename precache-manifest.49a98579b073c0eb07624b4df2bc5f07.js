c1ff955c4ac27fb1940ed8af302360aa8fa93c3c
Morteza Ansarinia <ansarinia@me.com>
2020-03-10 18:43:16 +0100

    Updates

---
+++ precache-manifest.49a98579b073c0eb07624b4df2bc5f07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a57278924ef51c3cea35807398f152f3",
+    "url": "/lens/index.html"
+  },
+  {
+    "revision": "d6ea154cae1161f5dd6d",
+    "url": "/lens/static/css/main.3631b8f3.chunk.css"
+  },
+  {
+    "revision": "4ec79e52eda23796cbda",
+    "url": "/lens/static/js/2.79c5fc11.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/lens/static/js/2.79c5fc11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6ea154cae1161f5dd6d",
+    "url": "/lens/static/js/main.bc0934bf.chunk.js"
+  },
+  {
+    "revision": "b388483deeeba0ec7e58",
+    "url": "/lens/static/js/runtime-main.85a3a999.js"
+  }
+]);
